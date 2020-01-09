67989318647ec0281bbe623af9120e98f24efb0c
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2020-01-09 15:10:07 -0600

    started working on setting up portfolio

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Route } from 'react-router-dom';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: '#26242D',
+        color: '#ffffff',
+    }
+}));
+
+export const App = () => {
+    const classes = useStyles();
+    return (
+        <div id='App' className={classes.root}>
+            <Route exact path='/' component={Landing} />
+        </div>
+    );
+};
