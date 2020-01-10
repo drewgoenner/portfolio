ab1b461d774759eeefe967ede2c9fe7902ec1de2
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2020-01-10 11:19:58 -0600

    started prepping for top nav

---
+++ landing/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link, Typographys, ButonBase } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+
+
+}));
+
+export default function Nav() {
+    const classes = useStyles();
+
+    
+}
